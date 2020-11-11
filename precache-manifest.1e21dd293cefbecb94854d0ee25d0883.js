83a5cd88b6ec2d89cca15694e974ec775c0c7473
Cyrus Xu <cyrus@overdose.digital>
2020-11-11 18:52:05 +1100

    Updates

---
+++ precache-manifest.1e21dd293cefbecb94854d0ee25d0883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7270e5605778c56e1746b9cdeb82d776",
+    "url": "/bike-management/index.html"
+  },
+  {
+    "revision": "63a82c2addc6cb1cc71f",
+    "url": "/bike-management/static/css/2.68aa1aa3.chunk.css"
+  },
+  {
+    "revision": "43951a6aa983f268cb57",
+    "url": "/bike-management/static/css/main.bea63e8a.chunk.css"
+  },
+  {
+    "revision": "63a82c2addc6cb1cc71f",
+    "url": "/bike-management/static/js/2.f4674044.chunk.js"
+  },
+  {
+    "revision": "e1f5ad522e1b15731a197887d22f1a58",
+    "url": "/bike-management/static/js/2.f4674044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43951a6aa983f268cb57",
+    "url": "/bike-management/static/js/main.8bba446f.chunk.js"
+  },
+  {
+    "revision": "6f4cc6efa9418dce2678",
+    "url": "/bike-management/static/js/runtime-main.21c20f49.js"
+  }
+]);
